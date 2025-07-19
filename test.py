e6490f2aefb6cb36454781d8d3c93946db86ef00
NatalieTJY <natalie1000tan@gmail.com>
2025-07-19 11:26:07 +0800

    Add files via upload

---
+++ test.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+st.header("Welcome to Crack Detection using Deep Learning")
+st.subheader("brought to you by Natalie Tan")
+
+st.button("Submit")
+st.image("merieducation-online-learning.gif")
+
+st.camera_input("Take a picture")
+
+st.date_input("Select the date")
+
+st.color_picker("Pick a colour")
+
+st.checkbox("Check")
+
+st.success("Preprocessing complete - 100%")
